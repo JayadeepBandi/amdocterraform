6a232f42b476a3b149644f36c03457320435f313
unknown <jayadeepbandi97@gmail.com>
2020-08-05 13:43:52 +0530

    Bootstarpe code added

--- main.tf
+++ main.tf
@@ -124,15 +124,30 @@ resource "azurerm_virtual_machine_scale_set" "web_server" {
             load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.web_server_lb_backend_pool.id]
         }
     }
+    extension {
+        name = "${local.web_server_name}-extension"
+        publisher = "Microsoft.Compute"
+        type = "CustomScriptExtension"
+        type_handler_version = "1.10"
+        settings = <<SETTINGS
+    {
+        "fileUris": "https://raw.githubusercontent.com/eltimmo/learning/master/azureInstallWebServer.ps1",
+        "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File azureInstallWebServer.ps1"
+    }
+SETTINGS
+
+    }
+
 }
+
 
 resource "azurerm_lb" "web_server_lb"{
     name = "${var.resource_prefix}-lb"
     location = var.web_server_location
     resource_group_name = azurerm_resource_group.web_server_rg.name
     frontend_ip_configuration {
-        name = "${var.resource_prefix}-lb-frontend-ip"
-        public_ip_address_id = azurerm_public_ip.web_server_ip.id
+        name                 = "${var.resource_prefix}-lb-frontend-ip"
+    public_ip_address_id = azurerm_public_ip.web_server_ip.id
 
     }
 }
