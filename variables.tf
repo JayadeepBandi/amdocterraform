4d5eab96d7854857717f7c993276afd451e847da
unknown <jayadeepbandi97@gmail.com>
2020-08-07 13:12:23 +0530

    Module Format

--- variables.tf
+++ variables.tf
@@ -1,18 +1,7 @@
-variable "web_server_location"{
-    type = string
-}
-
 variable "web_server_rg"{
     type = string
 }
 variable "resource_prefix"{
-    type = string
-}
-variable "web_server_address_space"{
-    type = string
-}
-
-variable "web_server_address_prefix"{
     type = string
 }
 
@@ -23,15 +12,9 @@ variable "web_server_name"{
 variable "environment" {
     type = string
 }
-
 variable "web_server_count"{
     type = string
 }
-
-variable "web_server_subnet"{
-    type = map
-}
-
 variable "terraform_script_version" {
     type = string
 }
